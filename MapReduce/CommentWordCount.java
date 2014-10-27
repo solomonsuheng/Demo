b35484335501d001b2bc950564f30f7d7bb12ba7
Solomonheng <solomonheng@yeah.net>
2014-10-27 10:13:57 +0800

    MapReduce
    WordCountDemo

---
+++ MapReduce/CommentWordCount.java
@@ -0,0 +1,75 @@
+package Chapter_1;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ * @author solomonheng
+ * @time 2014年10月1日 上午8:42:20
+ */
+public class CommentWordCount {
+	public static class WordCountMapper extends
+			Mapper<Object, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			Map<String, String> map = MRDPUtils.transformXmlToMap(value
+					.toString());
+			String txt = map.get("Text");
+			if (txt == null) {
+				return;
+			}
+
+			txt = txt.replace("'", "");
+			txt = txt.replaceAll("[^a-zA-Z]", " ");
+			StringTokenizer itr = new StringTokenizer(txt);
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+
+	public static class IntSumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws IOException,
+			ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "StackOverFlow Comment Word Count");
+		job.setJarByClass(CommentWordCount.class);// jar class
+		job.setMapperClass(WordCountMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
