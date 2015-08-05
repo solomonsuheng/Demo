eaca87b32d986db13d2fc6f91f63b73d83055954
solomon <1185626206@qq.com>
2015-08-05 18:13:58 +0800

    mvn WorkSpace

---
+++ mvnWorkSpace/src/main/java/HE.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Suheng on 8/5/15.
+ */
+public class HE {
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+}
---
+++ mvnWorkSpace/src/main/scala/io/transwarp/Demo.scala
@@ -0,0 +1,7 @@
+package io.transwarp
+
+object Demo {
+  def main(args: Array[String]) {
+    println("sd")
+  }
+}
